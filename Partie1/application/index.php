9fade483966aae050ae1a7b46d17872c76d619ac
JORDAN\Utilisateur <jordan.cote@outlook.com>
2020-11-02 08:34:29 -0500

    1er commit création des documents

---
+++ Partie1/application/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    define('PATH_ROOT', str_replace("application/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+    define('PATH_APPLICATION_ROOT', str_replace("index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+    define('PATH_VIEWS', PATH_APPLICATION_ROOT."Views");
+    define('PATH_CONTROLLERS', PATH_APPLICATION_ROOT."Controllers");
+    define('PATH_MODELS', PATH_APPLICATION_ROOT."Models");
+    define('PATH_DTO', PATH_APPLICATION_ROOT."DTO");
+    define('PATH_CORE', PATH_APPLICATION_ROOT."Core");
+    define('PATH_PARSER', PATH_APPLICATION_ROOT."Parser");
+    define('PATH_EXCEPTION', PATH_APPLICATION_ROOT."Exception");
+    define('PATH_PUBLIC', "Public");
+    require(PATH_APPLICATION_ROOT . 'dispatcher.php');
+
+    AuthenticationService::get_instance()->refresh_session();
+    $Dispatcher = new Dispatcher();
+    $Dispatcher->dispatch();
+?>
