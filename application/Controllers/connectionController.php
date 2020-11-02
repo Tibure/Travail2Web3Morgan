7628d189869259bd30f2bd8bd3163171d03883fc
JORDAN\Utilisateur <jordan.cote@outlook.com>
2020-11-02 09:52:31 -0500

    Home fait et debut de connection

---
+++ application/Controllers/connectionController.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once(PATH_CORE."/controller.php");
+
+    class ConnectionController extends Controller{
+        
+        const CONNECTION_TITLE = "Connexion";
+ 
+        public function __construct(){
+            
+        }
+
+        public function show(){
+            $view = new View("connectionView.php");
+            $data = array();
+            
+            $content = $view->render($data);
+            echo $this->render_template_with_content(self::CONNECTION_TITLE, $content);
+        }
+    }
+?>
--- application/Views/homeView.php
+++ application/Views/homeView.php
@@ -1,7 +1,23 @@
-<h1>Progression de la partie</h1>
+<h1>Progression des parties</h1>
 
-<div class="progress">
-  <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-</div>
+<!--
+  <h2>Equipe 1</h2>
+  <div class="progress">
+    <div class="progress-bar bg-warning" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0"
+      aria-valuemax="100">2/8</div>
+  </div>
 
-<!--<h2>En attente d'une partie</h2>-->
+
+  <h2>Equipe 2</h2>
+  <div class="progress">
+    <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+      aria-valuemax="100">8/8</div>
+  </div>
+
+  <h2>Equipe 3</h2>
+  <div class="progress">
+    <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0"
+      aria-valuemax="100">6/8</div>
+  </div>
+  -->
+<h2>En attente du lancement de votre partie...</h2>
