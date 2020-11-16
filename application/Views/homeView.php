a65ad11a95e2a7e18a32c53ddaaa9017fe4ed9b7
JORDAN\Utilisateur <jordan.cote@outlook.com>
2020-11-16 08:09:54 -0500

    Debut du cours du lundi 16 nov

--- application/Views/homeView.php
+++ application/Views/homeView.php
@@ -1,6 +1,6 @@
 <h1>Progression des parties</h1>
 
-<!--
+
   <h2>Equipe 1</h2>
   <div class="progress">
     <div class="progress-bar bg-warning" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0"
@@ -19,5 +19,5 @@
     <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0"
       aria-valuemax="100">6/8</div>
   </div>
-  -->
+
 <h2>En attente du lancement de votre partie...</h2>
